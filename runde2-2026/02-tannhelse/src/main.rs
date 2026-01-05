15d9916bec4a2f0ee43372d33c041a86ec2dbc67
LilleAila <olai.solsvik@gmail.com>
2026-01-05 10:37:54 +0100

    nio 02 (partial)

--- runde2-2026/02-tannhelse/src/main.rs
+++ runde2-2026/02-tannhelse/src/main.rs
@@ -1,3 +1,27 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let mut lines = io::stdin().lines();
+
+    let n: i32 = lines.next().unwrap().unwrap().trim().parse().unwrap();
+    let teeth: Vec<bool> = lines
+        .next()
+        .unwrap()
+        .unwrap()
+        .trim()
+        .split(" ")
+        .map(|x| x == "1")
+        .collect();
+
+    // println!("{} {:?}", n, teeth);
+
+    let mut potential: i32 = 0;
+    for (i, &t) in teeth.iter().enumerate() {
+        if t || (i > 0 && teeth[i - 1]) || (i < (n as usize) - 1 && teeth[i + 1]) {
+            potential += 1;
+        }
+    }
+
+    // TODO: find minimum. DP?
+    println!("{} {}", 0, potential);
 }
