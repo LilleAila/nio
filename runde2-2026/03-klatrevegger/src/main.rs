1343a6d0af3b70f527ffa5780034204e3153811c
LilleAila <olai.solsvik@gmail.com>
2026-01-06 14:02:04 +0100

    nio 03 attempt again

--- runde2-2026/03-klatrevegger/src/main.rs
+++ runde2-2026/03-klatrevegger/src/main.rs
@@ -1,5 +1,3 @@
-use std::cmp;
-use std::collections;
 use std::convert::TryInto; // 2021 edition compatibility (try_into) - it seems nio uses 2015
 use std::io;
 
@@ -8,7 +6,7 @@ type Graph = Vec<Vec<(usize, i32)>>;
 fn main() {
     let mut lines = io::stdin().lines();
 
-    let [n, k]: [i32; 2] = lines
+    let [n, k]: [usize; 2] = lines
         .next()
         .unwrap()
         .unwrap()
@@ -36,53 +34,32 @@ fn main() {
         graph[b].push((a, f as i32));
     }
 
-    let pairs: Vec<(usize, usize)> = (0..n)
-        .flat_map(|a| ((a + 1)..n).map(move |b| (a as usize, b as usize)))
-        .collect();
+    let mut dp: Vec<Vec<i32>> = vec![vec![i32::MAX; n]; n];
 
-    let mut weight = 0;
-    for (a, b) in pairs {
-        match dijkstra(&graph, a, b) {
-            Some(w) => {
-                weight = cmp::max(w, weight);
-            }
-            None => {
-                println!("-1");
-                return;
-            }
+    for u in 0..n {
+        dp[u][u] = 0;
+        for &(v, w) in &graph[u] {
+            dp[u][v] = w;
+            dp[v][u] = w;
         }
     }
-    println!("{}", weight);
-}
 
-fn dijkstra(graph: &Graph, start: usize, end: usize) -> Option<i32> {
-    let n = graph.len();
-    let mut dist = vec![i32::MAX; n];
-    let mut heap = collections::BinaryHeap::new();
-
-    dist[start] = 0;
-    heap.push(cmp::Reverse((0, start)));
-
-    while let Some(cmp::Reverse((cost, node))) = heap.pop() {
-        if node == end {
-            return Some(cost);
-        }
-
-        if cost > dist[node] {
-            continue;
-        }
-
-        for &(neighbor, weight) in &graph[node] {
-            // NOTE: The following line differs from standard dijkstra.
-            // It would have been `cost + weight` instead of `cmp::max(cost, weight)`
-            // But here we want the highest weight and not the total weight.
-            let next_cost = cmp::max(cost, weight);
-            if next_cost < dist[neighbor] {
-                dist[neighbor] = next_cost;
-                heap.push(cmp::Reverse((next_cost, neighbor)));
+    for k in 0..n {
+        for i in 0..n {
+            for j in 0..n {
+                let through_k = dp[i][k].max(dp[k][j]);
+                if through_k < dp[i][j] {
+                    dp[i][j] = through_k;
+                }
             }
         }
     }
 
-    None
+    let max: i32 = *dp.iter().flat_map(|xs| xs.iter().max()).max().unwrap();
+
+    if max == i32::MAX {
+        println!("-1");
+    } else {
+        println!("{}", max);
+    }
 }
