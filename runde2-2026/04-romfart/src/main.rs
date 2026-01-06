861514c235c41e900af99c427a5c4a315043c5de
LilleAila <olai.solsvik@gmail.com>
2026-01-06 17:54:49 +0100

    fix rust version

--- runde2-2026/04-romfart/src/main.rs
+++ runde2-2026/04-romfart/src/main.rs
@@ -1,5 +1,5 @@
 use std::convert::TryInto;
-use std::io;
+use std::io::{self, Write};
 
 fn main() {
     let mut lines = io::stdin().lines();
@@ -18,13 +18,18 @@ fn main() {
             .try_into()
             .unwrap();
 
+        // Binary search would be optimal if we didn't take cost into perspective
+        // This algorithm becomes more optimal as the limit of N approaches infinity.
+        // Due to this, it is likely the best for subtask 4 where N >= 30, but worse
+        // for the earlier ones where N has an upper bound.
         let mut a = a;
         let mut b = b;
         let mut k: i32;
         let mut success: i32 = 0;
         while success < n {
             k = (b + a) / 2;
-            println!("send {}", k);
+            println!("send {}", a);
+            io::stdout().flush().unwrap();
             let result = lines.next().unwrap().unwrap().trim() == "landet";
             if result {
                 b = k;
