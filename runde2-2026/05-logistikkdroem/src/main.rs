3b95c0c7f3ed8a34934dc640e4802f79a5270700
LilleAila <olai.solsvik@gmail.com>
2026-01-08 20:53:27 +0100

    idk

--- runde2-2026/05-logistikkdroem/src/main.rs
+++ runde2-2026/05-logistikkdroem/src/main.rs
@@ -1,6 +1,6 @@
 // Solution specifically for subtask 5 (all belts point upwards)
 
-use std::collections::HashSet;
+use std::collections::VecDeque;
 use std::convert::TryInto;
 use std::io;
 
@@ -18,7 +18,9 @@ fn main() {
         .try_into()
         .unwrap();
 
-    let mut belts: HashSet<(usize, usize)> = HashSet::new();
+    let index = |x, y| n * x + y;
+    let end = index(n - 1, n - 1);
+    let mut belts: Vec<bool> = vec![false; n * n];
 
     for _ in 0..k {
         let l = lines.next().unwrap().unwrap();
@@ -26,6 +28,58 @@ fn main() {
         let x: usize = l.next().unwrap().parse().unwrap();
         let y: usize = l.next().unwrap().parse().unwrap();
         l.next();
-        belts.insert((x - 1, y - 1));
+        belts[index(x, y)] = true;
     }
+
+    let mut dp = vec![u16::MAX; n * n];
+    dp[end] = 0;
+    let mut stack: VecDeque<usize> = VecDeque::new();
+    stack.push_back(end);
+
+    while let Some(a) = stack.pop_front() {
+        let c = dp[a];
+        let new_cost = c + 1;
+        let x = a % n;
+        let y = a / n;
+
+        if y + 1 < n {
+            let b = a + n;
+            if dp[b] > new_cost {
+                dp[b] = new_cost;
+                stack.push_back(b);
+            }
+        }
+
+        if y >= 1 {
+            let b = a - n;
+            let cost = if belts[b] { 0 } else { 1 };
+            let new_cost = c + cost;
+            if dp[b] > new_cost {
+                dp[b] = new_cost;
+            }
+            if c == 0 {
+                stack.push_front(b);
+            } else {
+                stack.push_back(b);
+            }
+        }
+
+        if x >= 1 {
+            let b = a - 1;
+            if dp[b] > new_cost {
+                dp[b] = new_cost;
+                stack.push_back(b);
+            }
+        }
+
+        if x + 1 < n {
+            let b = a + 1;
+            if dp[b] > new_cost {
+                dp[b] = new_cost;
+                stack.push_back(b);
+            }
+        }
+    }
+
+    println!("{}", dp[0]);
 }
